1a8d4f2efc3a19f209ff3671990482c62c39616d
sahara-yeh <55381730+sahara-yeh@users.noreply.github.com>
2019-09-16 16:50:19 +0300

    Delete account_report_add_journal.py

--- account_report_add_journal.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api,
-
-
-
-class account_report_add_journal(models.Model):
-	_name = "account.report.add.journal"
-    _description = "Add Journal"
-
-    name = fields.Char(translate=True)
