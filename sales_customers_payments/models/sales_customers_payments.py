9d9611f65de9d89bc14b6d23760abc76b7ca2988
med-sahara <55658971+med-sahara@users.noreply.github.com>
2019-09-23 17:30:53 +0300

    Update sales_customers_payments.py

--- sales_customers_payments/models/sales_customers_payments.py
+++ sales_customers_payments/models/sales_customers_payments.py
@@ -8,6 +8,7 @@ from odoo.addons import decimal_precision as dp
 class CustomersPaymentsSales(models.Model):
     _name = 'sales.customers.payments'
     _inherits = {'sale.order': 'partner_id'}
+    _description = "Sales Customers Payments"
 
 
 
@@ -16,7 +17,7 @@ class CustomersPaymentsSales(models.Model):
     name = fields.Char(readonly=True, copy=False) # The name is attributed upon post()
     state = fields.Selection([('draft', 'Draft'), ('confirmed', 'Confirmed'), ('sent', 'Sent'), ('reconciled', 'Reconciled'), ('cancelled', 'Cancelled')], readonly=True, default='draft', copy=False, string="Status")
 
-    partner_id = fields.Many2one('sale.order', string='Sale Order')
+    partner_id = fields.Many2one('sale.order', string='Sale Order', required=True)
 
     amount = fields.Monetary(string='Payment Amount', required=True)
     currency_id = fields.Many2one('res.currency', string='Currency', required=True, default=lambda self: self.env.user.company_id.currency_id)
